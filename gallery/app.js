af128ce28af2325aba542b80548bdbfe96e3c418
Eamonn Delaney <edelaney1992@gmail.com>
2022-04-06 21:07:18 +0100

    fixed some things

--- gallery/app.js
+++ gallery/app.js
@@ -1,18 +1,19 @@
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('image');
-const imageIndexes = [0,1,2,3];
+const imageIndexes = [0,1,2,3,4,5,6,7,8,9,10,11];
 const selectedIndex = null;
 
 imageIndexes.forEach(i => {
     const image = document.createElement('img');
-    image.src = `/img/liverpool-${i}.jpg`;
+    image.src = `https://ik.imagekit.io/eamonnd1/liverpool/tr:w-600/liverpool-${i}.jpg`;
+    // image.src = `/img/liverpool-${i}.jpg`;
     image.alt = `An image from my trip to Anfield`;
     image.classList.add('galleryImg');
 
     image.addEventListener('click', () => {
         popup.style.transform = `translateY(0)`;
-        selectedImage.src = `/img/liverpool-${i}.jpg`;
+        selectedImage.src = `https://ik.imagekit.io/eamonnd1/liverpool/tr:w-600/liverpool-${i}.jpg`;
     })
 
     gallery.appendChild(image);
@@ -23,3 +24,5 @@ popup.addEventListener('click', () => {
     popup.src = '';
     popup.alt = '';
 })
+
+// https://ik.imagekit.io/eamonnd1/liverpool/liverpool-11
